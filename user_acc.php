2b3bcc28a316c7662e9a3245e27227c3de50921f
ZuLu29 <markneilconde850@gmail.com>
2025-02-27 09:52:58 +0800

    `Added functions to generate encrypted password and user account data`

--- user_acc.php
+++ user_acc.php
@@ -3,3 +3,29 @@ require("vendor/autoload.php");
 
 use Ramsey\Uuid\Uuid;
 use Faker\Factory;
+
+$faker = Factory::create();
+
+function generateEncryptedPassword()
+{
+    $password = bin2hex(random_bytes(8));
+    return hash('sha256', $password);
+}
+function generateUserAccount($faker)
+{
+    $uuid = Uuid::uuid4()->toString();
+    $fullName = $faker->name;
+    $email = $faker->email;
+    $username = strtolower(explode('@', $email)[0]);
+    $password = generateEncryptedPassword();
+    $accountCreated = $faker->dateTimeBetween('-2 years', 'now')->format('Y-m-d H:i:s');
+
+    return [
+        'user_id' => $uuid,
+        'full_name' => $fullName,
+        'email' => $email,
+        'username' => $username,
+        'password' => $password,
+        'account_created' => $accountCreated,
+    ];
+};
