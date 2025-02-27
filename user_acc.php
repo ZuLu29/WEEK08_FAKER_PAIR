5f3628de829e905cf3886466a9bdfb732e873dc1
ZuLu29 <markneilconde850@gmail.com>
2025-02-27 10:07:44 +0800

    add html to display ouput

--- user_acc.php
+++ user_acc.php
@@ -33,3 +33,19 @@ $userAccounts = [];
 for ($i = 0; $i < 10; $i++) {
     $userAccounts[] = generateUserAccount($faker);
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
