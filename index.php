4a9227d02193eedef1c716e6ec58804a36bddc6c
ZuLu29 <markneilconde850@gmail.com>
2025-02-27 08:46:24 +0800

    adding array of job becuase faker jobs is not working

--- index.php
+++ index.php
@@ -6,13 +6,25 @@ $faker = Faker\Factory::create('en_PH');
 
 function generateProfile($faker)
 {
+    $jobTitles = [
+        'Software Engineer',
+        'Teacher',
+        'Accountant',
+        'Nurse',
+        'Doctor',
+        'Sales Manager',
+        'Graphic Designer',
+        'Marketing Specialist',
+        'Customer Service Representative',
+        'Data Analyst',
+    ];
     $profile = [
         'full_name' => $faker->name,
         'email' => $faker->email,
         'phone_number' => $faker->phoneNumber,
         'address' => $faker->address,
-        'birthdate' => $faker->date('yyyy-mm-dd'),
-        'job_title' => $faker->jobTitle
+        'birthdate' => $faker->date('Y-m-d'),
+        'job_title' => $faker->randomElement($jobTitles),
     ];
     $profile['phone_number'] = preg_replace('/^0/', '+63 ', $profile['phone_number']);
 
